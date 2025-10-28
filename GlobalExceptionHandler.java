cc5cebc4608992de21134bfefcc628fc254befe5
Kaio Meireles <145077870+Kaiomeireles@users.noreply.github.com>
2025-10-27 21:31:05 -0300

    Add GlobalExceptionHandler for centralized error handling

---
+++ GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.hotelreservation.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
